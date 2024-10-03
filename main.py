30f3bf17cb31e6de25a90d0d9a802ac5721cb0ce
Zemnuhov Egor <zemnuhov2405@gmail.com>
2024-10-03 15:34:32 +0300

    Add parallel port trigger

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
+from pathlib import Path
+import random
+
 from neuroexplib.exp_setting import ExperimentSetting
 from neuroexplib.experiment import Experiment
 from neuroexplib.stimulus_type import *
 
 if __name__ == '__main__':
+    stimulus = [path for path in Path("D:/Stimulus/Стимулы/pos").iterdir()] + [path for path in Path(
+        "D:/Stimulus/Стимулы/neg").iterdir()]
+    stimulus_list = []
+    for i in range(1, 201):
+        stimulus_path: Path = random.choice(stimulus)
+        stimulus_list.append(
+            ImageStimulus(
+                1000,
+                path=str(stimulus_path),
+                trigger_type=TriggerType.POSITIVE if "pos" in stimulus_path.parts else TriggerType.NEGATIVE, )
+        )
+        stimulus_list.append(TextStimulus(1000, '', 'white'))
+        if i % 4 == 0 and i != 0:
+            stimulus_list.append(ChoiceStimulus(10000, ['-3', '-2', '-1', '0', '1', '2', '3'], 'mouse'), )
+
     app = Experiment(
         ExperimentSetting(
-            stimulus=[
-                TextStimulus(1000, '+', 'white'),
-                VideoStimulus(1000, path='D:\Presentation\Stimuls/21.mp4', play_to_end=True),
-                ChoiceStimulus(10000, ['-3', '-2', '-1', '0', '1', '2', '3'], 'mouse'),
-                ImageStimulus(5000, path="D:\Presentation\Stimuls/1.jpg"),
-                ChoiceStimulus(10000, ['-3', '-2', '-1', '0', '1', '2', '3'], 'mouse'),
-                ImageStimulus(5000, path="D:\Presentation\Stimuls/2.jpg"),
-                ChoiceStimulus(10000, ['-3', '-2', '-1', '0', '1', '2', '3'], 'mouse'),
-                ImageStimulus(5000, path="D:\Presentation\Stimuls/3.jpg"),
-                ChoiceStimulus(10000, ['-3', '-2', '-1', '0', '1', '2', '3'], 'mouse'),
-            ],
+            stimulus=stimulus_list,
             default_delay=500,
             parallel_port_address=0x3EFC
         )
